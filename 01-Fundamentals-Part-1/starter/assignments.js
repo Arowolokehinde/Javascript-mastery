56054807fbc8e2d5ea8408c0dc5bcac7905ec33c
Arowoolo kehinde <arowolokehinde321@gmail.com>
2024-05-28 22:24:58 +0100

    learnt javascript

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,9 @@
+let country = "Nigeria";
+
+let continent = "Africa";
+
+let population = "300 million";
+
+console.log(country);
+console.log(continent);
+console.log(population);
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,99 @@
+// let js = "amazing";
+// if (js == "amazing") alert("javascript is fun!");
+
+// console.log(40 + 8 + 23 - 10);
+
+// console.log("Jonas");
+// console.log("23");
+
+// let firstName = "Matilda";
+
+// // console.log(firstName);
+
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+
+// // console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// // console.log(typeof 23);
+// // console.log(typeof "jonas");
+
+// javascriptIsFun = "YES!";
+// console.log(typeof javascriptIsFun);
+
+// year = 1982;
+// console.log(typeof year);
+
+// console.log(typeof null);
+
+// let age = 20;
+// age = 31;
+
+// const birthYear = 1991;
+// // birthYear = 1990;
+
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+// console.log(ageJonas, ageSarah);
+
+// console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+
+// const firstName = "Kehinde";
+// const lastName = "Arowolo";
+
+// console.log(firstName + " " + lastName);
+
+// let x = 10 + 5; //15
+// x += 10; // x = x + 10
+// console.log(x);
+
+// const firstName = "Kehinde";
+// const job = "teacher";
+// const birthYear = 1991;
+// const year = 2037;
+
+// const kehinde =
+//   "I'm" +
+//   " " +
+//   firstName +
+//   ", a " +
+//   (year - birthYear) +
+//   "years old" +
+//   job +
+//   "!";
+// console.log(kehinde);
+
+// const kehindeNew = `I'm ${firstName}, a ${year - birthYear} years old ${job} !`;
+// console.log(kehindeNew);
+
+// console.log(
+//   "String with \n\
+// multiple\n\
+// lines\n\
+// "
+// );
+
+// console.log(`String with
+// multiple
+// lines`);
+
+// const age = 15;
+
+// if (age >= 18) {
+//   console.log("Sarah can have her driving license 🚗");
+// } else {
+//   const yearsLeft = 18 - age;
+//   console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`);
+// }
+
+const birthYear = 1991;
+
+let century;
+if (birthYear <= 2000) {
+  century = 20;
+} else {
+  century = 21;
+}
+
+console.log(century);

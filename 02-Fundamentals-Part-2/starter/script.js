f1bc7b71cc380c6b36a8b1c71f26f50c9e2a3f40
Arowoolo kehinde <arowolokehinde321@gmail.com>
2024-05-31 19:58:30 +0100

    work

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -40,26 +40,40 @@
 // const age3 = calcAge3(1991);
 // console.log(age3);
 
-// const yearsUntilRetirement = (birthYear, firstName) => {
-//   const age = 2037 - birthYear;
-//   const retirement = 65 - age;
-//   return `${firstName} retires in ${retirement} years`;
-// };
-
 // console.log(yearsUntilRetirement(1991, "Jonas"));
 // console.log(yearsUntilRetirement(1980, "Bob"));
 
-function cutFruitPieces(fruit) {
-  return fruit * 4;
-}
+// function cutFruitPieces(fruit) {
+//   return fruit * 4;
+// }
 
-function fruitProcessor(apples, oranges) {
-  const applePieces = cutFruitPieces(apples);
-  const orangePieces = cutFruitPieces(oranges);
+// function fruitProcessor(apples, oranges) {
+//   const applePieces = cutFruitPieces(apples);
+//   const orangePieces = cutFruitPieces(oranges);
 
-  // console.log(apples, oranges);
-  const juice = `Juice with ${applePieces} apples and ${orangePieces} orangees`;
-  return juice;
-}
+//   // console.log(apples, oranges);
+//   const juice = `Juice with ${applePieces} apples and ${orangePieces} orangees`;
+//   return juice;
+// }
 
-console.log(fruitProcessor(2, 3));
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+// console.log(fruitProcessor(2, 3));
+
+const yearsUntilRetirement = function (birthYear, firstName) {
+  const age = calcAge(birthYear);
+  const retirement = 65 - age;
+
+  if (retirement > 0) {
+    console.log(`${firstName} retires in ${retirement} years`);
+    return retirement;
+  } else {
+    console.log(`${firstName} has already retired`);
+    return -1;
+  }
+};
+
+console.log(yearsUntilRetirement(1991, "Jonas"));
+console.log(yearsUntilRetirement(1950, "Mike"));
